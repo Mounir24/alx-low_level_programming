ddfb08df18603d576dc42faa1127ea486962d4e3
= <=>
2023-02-27 12:54:24 -0800

    Task#9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/*-- More headers goeshere --*/
+
+/**
+ * main - Prints All possible comninations of single-digit numbers.
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}

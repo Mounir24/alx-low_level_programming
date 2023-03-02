f7c7c99f6ce53718dcf82e026af851c214891178
= <=>
2023-03-02 07:18:09 -0800

    Task#3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,10 +16,10 @@ char *_strncpy(char *dest, char *src, int n)
 		*(dest + i) = *(src + i);
 		i++;
 	}
-	if (*(src + i) == '\n')
+	if (*(src + i) == '\0')
 	{
 		while (i < n)
-			*(dest + i++) = '\n';
+			*(dest + i++) = '\0';
 	}
 
 	return (dest);

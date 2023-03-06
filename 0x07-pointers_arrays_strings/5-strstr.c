183a02c5d606e222452016049d172acfcc4ed267
= <=>
2023-03-06 13:34:22 -0800

    Task#5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  * _strstr - function
+  * @haystack : variable
+  * @needle : variable
+  * Return: return
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *Begin = haystack;
+		char *pattern = needle;
+
+		while (*haystack && *pattern && *haystack == *pattern)
+		{
+			haystack++;
+			pattern++;
+		}
+
+		if (!*pattern)
+		{
+			return (Begin);
+		}
+
+		haystack = Begin + 1;
+	}
+
+	return (0);
+}

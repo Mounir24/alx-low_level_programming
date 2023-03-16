ff5ffc32cc21218a274b369fae17dd4645a03609
= <=>
2023-03-16 09:44:23 -0700

    Task#2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,6 +21,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	ptr = malloc(nmemb * size);
 
+	if (ptr == NULL)
+		return (NULL);
+
 	for (i = 0; i < (nmemb * size); i++)
 	{
 		ptr[i] = 0;

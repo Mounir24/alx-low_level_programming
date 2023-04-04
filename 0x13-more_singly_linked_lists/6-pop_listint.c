2901bb8a88ddfa095f0935a8e63f94b50e5b327c
= <=>
2023-04-04 10:05:23 -0700

    Task#6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,23 +1,24 @@
 #include "lists.h"
+
 /**
-  *pop_listint - remove the head of a linked list
-  *@head : the head of a linked list
-  *Return: the value in the list
-  */
+ *pop_listint - deletes the head node of a linked list
+ *@head: head of the list
+ *
+ *Return: the deleted node data
+ */
 
 int pop_listint(listint_t **head)
 {
-	int n;
-	listint_t *t;
+	listint_t *popped;
+	int content;
 
-	if (!*head)
+	if (*head == NULL)
 		return (0);
-	else
-	{
-		t = *head;
-		n = (t)->n;
-		free(t);
-	}
 
-	return (n);
+	popped = *head;
+	content = popped->n;
+	free(popped);
+
+	*head = (*head)->next;
+	return (content);
 }
